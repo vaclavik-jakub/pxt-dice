d63f4b945682104fbaff1b3e372eeba981c33ba0
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2022-11-24 10:28:56 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
 input.onButtonPressed(Button.A, function () {
-    Základní_číslo += 1
+	
 })
-input.onButtonPressed(Button.B, function () {
-    Základní_číslo += -1
-})
-let Základní_číslo = 0
-basic.showNumber(Základní_číslo)
-Základní_číslo = 0
