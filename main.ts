19efa65e38d35b46f1b88fbd009af5134c2d512b
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2022-11-24 11:35:14 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
+let number = 0
+basic.showNumber(number)
+
+basic.forever(function() {
+    if (input.buttonIsPressed(Button.A)){
+        number -= 1
+        whaleysans.showNumber(number)
+        basic.pause(200)
+    }
+    if (input.buttonIsPressed(Button.B)){
+        (number >= 10)
+    }
+        number += 1
+    }
+    
+
+// input.onButtonPressed(Button.A, function() {
+//     if (number > 0)
+//     {
+//         number -= 1
+//     }
+//     whaleysans.showNumber(number)
+// })
+    
+// input.onButtonPressed(Button.B, function() {
+//     if (number < 10) {
+//         number += 1
+
+//     }
+//     whaleysans.showNumber(number)
+// })
+
+
+   
+
+
+
+
+
+
+
+
