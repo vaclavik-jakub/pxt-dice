094b0ee2545c75db924a422d28628a3c047b578c
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2022-11-24 10:25:14 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    Základní_číslo += 1
+})
+input.onButtonPressed(Button.B, function () {
+    Základní_číslo += -1
+})
+let Základní_číslo = 0
+basic.showNumber(Základní_číslo)
+Základní_číslo = 0
