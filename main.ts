7a8f00e534c52e0896a14be51a42a546fdc8e4dc
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2022-12-08 11:39:22 +0100

    stopky

--- main.ts
+++ main.ts
@@ -1,44 +1,21 @@
-let number1 = 4
-let previewStateAPressedA = input.buttonIsPressed(Button.A)
-let previewStateAPressedB = input.buttonIsPressed(Button.B)
-
-let makej2A = () => {
-    number1 -= 1
-    whaleysans.showNumber(number1)
-}
-
-let makej2B = () => {
-    number1 += 1
-    whaleysans.showNumber(number1)
-}
+let bornin = control.millis()
+let previewStateAPressed = input.buttonIsPressed(Button.A)
 
 let makej = function () { 
     if (input.buttonIsPressed(Button.A)) {
-        //let actualStateA = true
-       //if (previewStateAPressed ==  false && actualStateA == true)
-        if (!previewStateAPressedA) 
+        if (!previewStateAPressed) 
         {
-            number1 -= 1
-            previewStateAPressedA = true
+            bornin = control.millis()
+            previewStateAPressed = true
         }
     } else {
-        previewStateAPressedA = false
+        if (previewStateAPressed) {
+            basic.showNumber (control.millis() - bornin)
+            previewStateAPressed = false
+        }
     }
-
-    if (input.buttonIsPressed(Button.B))
-        
-        if (!previewStateAPressedB)
-    {
-        number1 += 1
-        previewStateAPressedB = true 
-    } 
-    else {
-        previewStateAPressedB = false
-    }
-    whaleysans.showNumber(number1)
 }
 
-
-input.onButtonPressed(Button.A, makej2A) //registrace k události
-input.onButtonPressed(Button.B, makej2B)
-
+while (true) { 
+    makej()
+}
